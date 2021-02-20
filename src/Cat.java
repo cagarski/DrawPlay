2d59c3d622983d0a77c309c5bd88e2f746b02ef3
cagarski <claregarski@mymail.mines.edu>
2021-02-20 11:26:05 -0700

    changed color to grey, changed text to HISS!

--- src/Cat.java
+++ src/Cat.java
@@ -50,10 +50,10 @@ public class Cat {
 		x=catX;
 		y=catY;
 		// Draw the head
-		g2.setColor(Color.yellow);
+		g2.setColor(Color.darkGray);
 		g2.fillOval(x, y, HEAD_DIMENSION, HEAD_DIMENSION);
 		// Draw the eyes
-		g2.setColor(Color.green);
+		g2.setColor(Color.red);
 		x = catX + EYE_X; 
 		y = catY + EYE_Y;
 		g2.fillOval(x, y, EYE_WIDTH, EYE_HEIGHT);
@@ -75,6 +75,6 @@ public class Cat {
 		g2.setColor(Color.red);
 		// Meow text appears below cat head, +10 places below 
 		// so it doesn't overlap the drawing
-		g2.drawString("Meow Meow", catX, catY+HEAD_DIMENSION+10);	
+		g2.drawString("HISS!", catX, catY+HEAD_DIMENSION+10);	
 	}
 }
