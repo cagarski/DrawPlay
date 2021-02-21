1adab504c018213b99a1c437e4e1e782c5761c37
unknown <ethanwt01@gmail.com>
2021-02-20 18:23:21 -0700

    added nose and hat

--- src/Cat.java
+++ src/Cat.java
@@ -20,12 +20,35 @@ public class Cat {
 	private static final int MOUTH_X = HEAD_DIMENSION/4 * 2;
 	private static final int MOUTH_Y = HEAD_DIMENSION/5 * 3;
 	
+	// pick nose height, width is based on head dimension
+	private static final int NOSE_HEIGHT = 30;
+	private static final int NOSE_WIDTH = HEAD_DIMENSION/4;
+		
+	private static final int NOSE_X = HEAD_DIMENSION/4 * 2;
+	private static final int NOSE_Y = HEAD_DIMENSION/5 * 3 - 30;
+	
+	// pick hat height, width is based on head dimension
+	private static final int HAT_HEIGHT = 40;
+	private static final int HAT_WIDTH = HEAD_DIMENSION/2;
+			
+	private static final int HAT_X = HEAD_DIMENSION/4;
+	private static final int HAT_Y = -20 ;
+		
+		
 	// draw will render the Cat on the Graphics object
 	public void draw(Graphics g, int catX, int catY)
 	{
 		Graphics2D g2 = (Graphics2D) g;
-		int x=catX;
-		int y=catY;
+		
+		// Draw the hat
+		g2.setColor(Color.ORANGE);
+		int x = catX + HAT_X;
+		int y = catY + HAT_Y;
+		g2.fillOval(x, y, HAT_WIDTH, HAT_HEIGHT);
+						
+				
+		x=catX;
+		y=catY;
 		// Draw the head
 		g2.setColor(Color.yellow);
 		g2.fillOval(x, y, HEAD_DIMENSION, HEAD_DIMENSION);
@@ -41,6 +64,14 @@ public class Cat {
 		x = catX + MOUTH_X;
 		y = catY + MOUTH_Y;
 		g2.fillOval(x, y, MOUTH_WIDTH, MOUTH_HEIGHT);
+		
+		// Draw the mouth
+		g2.setColor(Color.red);
+		x = catX + NOSE_X;
+		y = catY + NOSE_Y;
+		g2.fillOval(x, y, NOSE_WIDTH, NOSE_HEIGHT);
+		
+					
 		g2.setColor(Color.red);
 		// Meow text appears below cat head, +10 places below 
 		// so it doesn't overlap the drawing
